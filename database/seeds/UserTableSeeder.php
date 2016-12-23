f8baa2a952501eea620de51380751dace84d3100
fernando.arantes <fernando.arantes@ifto.edu.br>
2016-12-22 22:12:50 -0300

    Criação ProjectTask

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -14,12 +14,12 @@ class UserTableSeeder extends Seeder
         //
         //Project::truncate();
         
-       /* factory(\pjLaravel\Entities\User::class)->create([
+       factory(\pjLaravel\Entities\User::class)->create([
             'name' => 'Fernando de Souza Arantes',
             'email' => 'fernando.arantes@ifto.edu.br',
             'password' => bcrypt('buchin26'),
             'remember_token' => str_random(10),
-        ]);*/
+        ]);
         
         factory(\pjLaravel\Entities\User::class, 5)->create();
     }
